d5ce4e82ebe415c8d4a4d9f8bcbcb363cdc2d246
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-07-21 22:07:38 -0500

    added nre files and folders

---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+// app.get("/api/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/public/notes.htmll"));
+// });
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require('path')
+require('./routes/routes')
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
